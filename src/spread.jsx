518a71983313fb8a855babb324b031c7cc8fa5b3
Ankur1234b <singhankurkumar369@gmail.com>
2024-03-17 12:25:47 +0530

    first commit

---
+++ src/spread.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Spread() {
+  return (
+    <div>
+      const arr=[1,2,3,4,5,{id:1}]
+      console.log(...arr);
+    </div>
+  )
+}
+
+export default Spread
+
